c11432c95e114d59c69de69fdca00112bc1f387c
Jeff <jeff41101@gmail.com>
2021-04-20 23:51:02 +0800

    add user index page with follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,29 @@ before_action :set_user, only: [:show, :edit, :update]
     @commented_restaurants = @user.restaurants.uniq
   end
 
+  def index
+     @users = User.all
+  end
+
   def edit
-     unless @user == current_user
-       redirect_to user_path(@user)
-     end
-   end
-
-   def update
-     @user.update(user_params)
+   unless @user == current_user
      redirect_to user_path(@user)
-   end
+    end
+  end
 
-   private
+  def update
+   @user.update(user_params)
+   redirect_to user_path(@user)
+  end
+
+  private
 
    def set_user
      @user = User.find(params[:id])
    end
 
    def user_params
-params.require(:user).permit(:name, :intro, :avatar)
+     params.require(:user).permit(:name, :intro, :avatar)
    end
 
 end
