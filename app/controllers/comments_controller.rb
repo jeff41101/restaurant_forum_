7f723b7c7d597d6f2614eacdf8cac5d8da4d6d57
Jeff <jeff41101@gmail.com>
2021-04-20 21:05:49 +0800

    show commented restaurants on user profile

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,19 @@ class CommentsController < ApplicationController
       redirect_to restaurant_path(@restaurant)
     end
   end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = @restaurant.comments.build(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+  
 end
