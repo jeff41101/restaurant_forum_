6e4d2f9e8efd2a36dcfb695fc4504ed24136725a
Jeff <jeff41101@gmail.com>
2021-04-20 21:49:33 +0800

    create favorite model and setup relation among user, restaurant and favorite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210420122444) do
+ActiveRecord::Schema.define(version: 20210420133458) do
 
   create_table "add_attributes_to_users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20210420122444) do
     t.text "content"
     t.integer "restaurant_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
